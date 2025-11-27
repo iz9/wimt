55c9d5b6a815142f8581450e79716800c1707198
iz9 <dev.iz9.l.n@gmail.com>
2025-11-27 12:44:54 +0200

    #11: abstract & interface

---
+++ packages/domain/src/specifications/Specification.ts
@@ -0,0 +1,93 @@
+/**
+ * Base Specification interface
+ * Encapsulates business rules for selecting/filtering domain objects
+ */
+export interface Specification<T> {
+  /**
+   * Check if candidate satisfies this specification
+   */
+  isSatisfiedBy(candidate: T): boolean;
+
+  /**
+   * Combine specifications with AND logic
+   */
+  and(other: Specification<T>): Specification<T>;
+
+  /**
+   * Combine specifications with OR logic
+   */
+  or(other: Specification<T>): Specification<T>;
+
+  /**
+   * Negate specification
+   */
+  not(): Specification<T>;
+}
+
+/**
+ * Abstract base class with composition logic
+ */
+export abstract class CompositeSpecification<T> implements Specification<T> {
+  abstract isSatisfiedBy(candidate: T): boolean;
+
+  and(other: Specification<T>): Specification<T> {
+    return new AndSpecification(this, other);
+  }
+
+  or(other: Specification<T>): Specification<T> {
+    return new OrSpecification(this, other);
+  }
+
+  not(): Specification<T> {
+    return new NotSpecification(this);
+  }
+}
+
+/**
+ * AND combination of two specifications
+ */
+class AndSpecification<T> extends CompositeSpecification<T> {
+  constructor(
+    private readonly left: Specification<T>,
+    private readonly right: Specification<T>,
+  ) {
+    super();
+  }
+
+  isSatisfiedBy(candidate: T): boolean {
+    return (
+      this.left.isSatisfiedBy(candidate) && this.right.isSatisfiedBy(candidate)
+    );
+  }
+}
+
+/**
+ * OR combination of two specifications
+ */
+class OrSpecification<T> extends CompositeSpecification<T> {
+  constructor(
+    private readonly left: Specification<T>,
+    private readonly right: Specification<T>,
+  ) {
+    super();
+  }
+
+  isSatisfiedBy(candidate: T): boolean {
+    return (
+      this.left.isSatisfiedBy(candidate) || this.right.isSatisfiedBy(candidate)
+    );
+  }
+}
+
+/**
+ * NOT negation of a specification
+ */
+class NotSpecification<T> extends CompositeSpecification<T> {
+  constructor(private readonly spec: Specification<T>) {
+    super();
+  }
+
+  isSatisfiedBy(candidate: T): boolean {
+    return !this.spec.isSatisfiedBy(candidate);
+  }
+}
